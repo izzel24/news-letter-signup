215e11f43e1c34b5a2779d6d13d3ad2cb551074b
izzel24 <izzzzzzell2401@gmail.com>
2023-11-14 16:24:44 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+form1.addEventListener("submit",(e) => {
+    e.preventDefault();
+    err();
+});
+
+function err(){
+    let notiferr = document.getElementById("error");
+    let input = form1.email.value;
+    let correctformat = input.search("@");
+    if(input === "" || correctformat === -1 ){
+    notiferr.innerText = "Valid email required";
+}else{
+    window.location.replace("/page2.html")
+}
+
+}
